5e1119875442d8e3983b50bd68b421f08bb48510
HardikMohite <hardik.mohite01@gmail.com>
2025-12-09 12:00:26 +0530

    Added Day02 Python programs (Typecasting)

---
+++ Day02/typecasting.py
@@ -0,0 +1,6 @@
+age = input("Enter your age: ")
+age = int(age)   # converting string to integer
+print("Next year you will be", age + 1)
+# also you can write code like this 
+#age = int(input("Enter your age: "))
+#print("Next year you will be", age + 1)
