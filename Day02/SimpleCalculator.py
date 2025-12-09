de01fe5d5b8b89a058ac613509e52a1f0cfee5f3
HardikMohite <hardik.mohite01@gmail.com>
2025-12-09 12:03:05 +0530

    Added Day02 Python programs (Calculator)

---
+++ Day02/SimpleCalculator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)        # gives decimal result
+print("Remainder =", a % b)       # modulo
+print("Power =", a ** b)          # a raised to b
