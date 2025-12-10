e1e14fcd07aa8b80dec3b5ab23f709fc0d118c5b
HardikMohite <hardik.mohite01@gmail.com>
2025-12-10 23:43:08 +0530

    Day 03 Python programs - Operators

---
+++ Day03/ComparisonOperator.py
@@ -0,0 +1,12 @@
+x = 10
+y = 20
+
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y:", x > y)
+print("x < y:", x < y)
+print("x >= y:", x >= y)
+print("x <= y:", x <= y)
+
+# try changing values it will help you to understand program more deeply 
+# e.g change x = 20 , x=30 etc 
