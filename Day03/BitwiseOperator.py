f65ac4602e8b8d915c35bfb77c8b85955bfc934f
HardikMohite <hardik.mohite01@gmail.com>
2025-12-10 23:55:23 +0530

    Day 03 Python programs - Operators

---
+++ Day03/BitwiseOperator.py
@@ -0,0 +1,9 @@
+a = 5   # binary: 0101
+b = 3   # binary: 0011
+
+print("a & b:", a & b)   # AND
+print("a | b:", a | b)   # OR
+print("a ^ b:", a ^ b)   # XOR
+print("~a:", ~a)         # NOT
+print("a << 1:", a << 1) # Left Shift
+print("a >> 1:", a >> 1) # Right Shift
