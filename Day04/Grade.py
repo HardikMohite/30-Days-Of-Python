05943fe5b0e461c849e82f28b4feec852d3537c0
HardikMohite <hardik.mohite01@gmail.com>
2025-12-11 19:56:52 +0530

    Day 04 Python programs using if, else and elif statements

---
+++ Day04/Grade.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter the marks ="))
+
+if(marks>=90 and marks<100):
+    print("Grade A")
+elif(marks>=80):
+    print("Grade B")
+elif(marks>=70):
+    print("Grade C")
+elif(marks>=60):
+    print("Grade D")
+elif(marks>=35):
+    print("Grade E")
+else:
+    print("Grade F")
